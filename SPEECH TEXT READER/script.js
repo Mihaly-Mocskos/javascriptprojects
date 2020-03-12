510bc874f795fe87889b04ddec2713a04bf62d5c
Mocskos Mihaly <mocskos.misi@gmail.com>
2020-03-12 10:42:15 +0100

    making a project called speech text reader

---
+++ SPEECH TEXT READER/script.js
@@ -0,0 +1,161 @@
+const main = document.querySelector('main');
+const voiceSelect = document.getElementById('voices');
+const textarea = document.getElementById('text');
+const readBtn = document.getElementById('read');
+const toggleBtn = document.getElementById('toggle');
+const closeBtn = document.getElementById('close');
+
+
+const data = [
+    {
+        image: './img/drink.jpg',
+        text: "I'm  sad "
+    },
+    {
+        image: './img/food.jpg',
+        text: "I'm Hungry"
+    },
+    {
+        image: './img/tired.jpg',
+        text: "I'm Tired"
+    },
+    {
+        image: './img/hurt.jpg',
+        text: "I'm Hurt"
+    },
+    {
+        image: './img/happy.jpg',
+        text: "I'm Happy"
+    },
+    {
+        image: './img/angry.jpg',
+        text: "I'm Angry"
+    },
+    {
+        image: './img/sad.jpg',
+        text: "I'm Sad"
+    },
+    {
+        image: './img/scared.jpg',
+        text: "I'm Scared"
+    },
+    {
+        image: './img/outside.jpg',
+        text: 'I Want To Go Outside'
+    },
+    {
+        image: './img/home.jpg',
+        text: 'I Want To Go Home'
+    },
+    {
+        image: './img/school.jpg',
+        text: 'I Want To Go To School'
+    },
+    {
+        image: './img/grandma.jpg',
+        text: 'I Want To Go To Grandmas'
+    }
+];
+
+
+data.forEach(createBox);
+
+
+function createBox(item) {
+    const box = document.createElement('div');
+    const { image, text } = item;   //item.image item.text helyett
+    box.classList.add('box');
+    box.innerHTML = `
+    <img src="${image}" alt="${text}"/>
+    <p class="info">${text}</p>
+    `;
+
+    box.addEventListener('click', () => {
+        setTextMessage(text);
+        speakText();
+
+
+        //add active effect#
+
+
+        box.classList.add('active');
+        setTimeout(() => box.classList.remove('active'), 800);
+
+    });
+
+
+
+
+
+
+
+
+
+
+    main.appendChild(box);
+
+
+}
+
+//init speech synth
+
+const message = new SpeechSynthesisUtterance();
+
+
+
+//create an array to store voices
+let voices = [];
+
+
+
+function getVoices() {
+    voices = speechSynthesis.getVoices();
+    voices.forEach(voice => {
+        const option = document.createElement('option');
+        option.value = voice.name;
+        option.innerText = `${voice.name}  ${voice.lang}`;
+
+        voiceSelect.appendChild(option);
+    });
+}
+
+//set the text
+
+function setTextMessage(text) {
+    message.text = text;
+
+}
+
+function speakText() {
+    speechSynthesis.speak(message);
+}
+
+function setVoice(e) {
+    message.voice = voices.find(voice => voice.name === e.target.value);
+}
+
+
+speechSynthesis.addEventListener('voiceschanged', getVoices);
+
+
+
+
+
+
+
+toggleBtn.addEventListener('click', () => document.getElementById('text-box').classList.toggle('show'));
+
+
+
+closeBtn.addEventListener('click', () => document.getElementById('text-box').classList.remove('show'));
+
+voiceSelect.addEventListener('change', setVoice);
+
+readBtn.addEventListener('click', () => {
+    setTextMessage(textarea.value);
+    speakText();
+})
+
+
+
+getVoices();
